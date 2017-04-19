9d68bc986ede5e81c62801071e8eec62ce6ab2c2
gr8seapr0o <gr8seapr0o@users.noreply.github.com>
2017-04-19 12:12:58 +0300

    Update chetnye.php

--- arrays_loops_tasks/chetnye.php
+++ arrays_loops_tasks/chetnye.php
@@ -14,3 +14,26 @@ for ($i = 2; $i <= $s;$i++){
     }
     var_dump($arr);
 }
+
+//по решету Эратосфена
+
+
+define("LIMIT", 1000);
+define("SQRT_LIMIT",floor(sqrt(LIMIT)));
+
+$s = array_fill(2,LIMIT-1,true);
+
+for($i=2;$i<=SQRT_LIMIT;$i++){
+if($s[$i]===true){
+for($j=$i*$i; $j<=LIMIT; $j+=$i){
+$s[$j]=false;
+}
+}
+}
+for($n=2; $n<=count($s); $n++)
+{
+if($s[$n]) 
+{
+echo ' '.$n; 
+}
+}

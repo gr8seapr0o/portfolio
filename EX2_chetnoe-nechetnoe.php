6f72a0d97d79afa877461432bb912c1f0cc5ad71
gr8seapr0o <gr8seapr0o@users.noreply.github.com>
2017-04-19 14:34:22 +0300

    Add files via upload

---
+++ EX2_chetnoe-nechetnoe.php
@@ -0,0 +1,35 @@
+<?php
+/*
+function my_func($num) {
+    if($num % 2 == 0) {
+        return $num;
+    }
+    else {
+        return false;//не четное
+    }
+}*/
+$arr = [1, 2, 7, 4, 9, 5, 6, 10];
+sort($arr);
+var_dump($arr);
+$i = 0;
+$j =0;
+while ($i <= (count($arr)-1))
+{
+    if($arr[$i] % 2 !== 0 ) {
+        $arr1[$j]['odd'] = $arr[$i];
+    } else {
+        $arr1[$j]['odd'] = null;
+    }
+    $i++;
+    if($arr[$i] % 2 == 0) {
+        $arr1[$j]['even'] = $arr[$i];
+    } else {
+        $arr1[$j]['even'] = null;
+    }
+
+    $arr1[$j]['sum'] = $arr1[$j]['odd'] + $arr1[$j]['even'];
+
+    $j++;
+}
+
+var_dump($arr1);

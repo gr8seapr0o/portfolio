3882661da8afc802cb970d701cb5a46ac3d651ae
gr8seapr0o <mon.to.burn@gmail.com>
2017-04-19 16:12:51 +0300

    444

--- EX2_chetnoe-nechetnoe.php
+++ EX2_chetnoe-nechetnoe.php
@@ -1,32 +1,27 @@
 <?php
-/*
-function my_func($num) {
-    if($num % 2 == 0) {
-        return $num;
-    }
-    else {
-        return false;//не четное
-    }
-}*/
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
+
 $arr = [1, 2, 7, 4, 9, 5, 6, 10];
 sort($arr);
 var_dump($arr);
 $i = 0;
-$j =0;
-while ($i <= (count($arr)-1))
+$j = 0;
+$k = '';
+while ($i < (count($arr)- 1))
 {
     if($arr[$i] % 2 !== 0 ) {
         $arr1[$j]['odd'] = $arr[$i];
     } else {
-        $arr1[$j]['odd'] = null;
+        $arr1[$j]['odd'] = $k;
     }
     $i++;
     if($arr[$i] % 2 == 0) {
         $arr1[$j]['even'] = $arr[$i];
     } else {
-        $arr1[$j]['even'] = null;
+        $arr1[$j]['even'] = $k;
     }
-
+if ($arr1[$j]['even'] &&  $arr1[$j]['odd'] == $k )
     $arr1[$j]['sum'] = $arr1[$j]['odd'] + $arr1[$j]['even'];
 
     $j++;

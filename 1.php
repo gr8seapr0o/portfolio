28cb073d8e4820df35d5cee85753e5eb758be3a5
gr8seapr0o <gr8seapr0o@users.noreply.github.com>
2017-04-19 17:26:28 +0300

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,26 @@
+<?php
+$arr = [1, 2, 7, 4, 9, 5, 6, 10];
+sort($arr);
+var_dump($arr);
+$i = 0;
+$j =0;
+while ($i <= (count($arr)-1))
+{
+    if($arr[$i] % 2 !== 0 ) {
+        $arr1[$j]['odd'] = $arr[$i];
+    } else {
+        $arr1[$j]['odd'] = null;
+    }
+    $i++;
+    if($arr[$i] % 2 == 0) {
+        $arr1[$j]['even'] = $arr[$i];
+    } else {
+        $arr1[$j]['even'] = null;
+    }
+
+    $arr1[$j]['sum'] = $arr1[$j]['odd'] + $arr1[$j]['even'];
+
+    $j++;
+}
+
+var_dump($arr1);

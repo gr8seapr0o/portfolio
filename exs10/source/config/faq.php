d4a467444f8ca065bc848184360a975fd1cc9669
gr8seapr0o <mon.to.burn@gmail.com>
2017-05-03 17:39:18 +0300

    17-39

--- exs10/source/config/faq.php
+++ exs10/source/config/faq.php
@@ -2,9 +2,9 @@
 <?php
 
 $faq["2017-05-03 04:42:48"] = array (
-  'firstname' => 'Виктор',
-  'email' => 'mon.to.burn@gmail.com',
-  'message' => 'wer',
+  'firstname' => 'Сергей',
+  'email' => 'sergey@gmail.com',
+  'message' => 'Нужны ли какие-то специальные знания и умения?',
 );
 $faq["2017-05-03 04:49:44"] = array (
   'firstname' => 'Виктор',
@@ -57,17 +57,36 @@ $faq["2017-05-03 05:29:49"] = array (
   'message' => 'www',
 );
 $faq["2017-05-03 05:30:53"] = array (
-  'firstname' => 'Виктор',
-  'email' => 'mon.to.burn@gmail.com',
-  'message' => 'www',
+  'firstname' => 'Михаил',
+  'email' => 'Mihail@ukr.com',
+  'message' => 'Как принять участие в игре?',
+    'request' => [
+        '2017-05-02 15:30:04'=>[
+            'firstname' => '***',
+            'message' => 'Вы можете забронировать игру на сайте, или по телефону, или придя на одну из наших локаций.',
+        ],
+    ],
 );
 $faq["2017-05-03 05:31:19"] = array (
-  'firstname' => 'Виктор',
-  'email' => 'mon.to.burn@gmail.com',
-  'message' => 'www',
+  'firstname' => 'Алена',
+  'email' => 'Alena@gmail.com',
+  'message' => 'А если мы не сможем решить какую-то головоломку?',
+    'request' => [
+        '2017-05-02 15:30:04'=>[
+            'firstname' => '***',
+            'message' => 'Наши операторы всегда готовы прийти  вам на помощь. Нажмите на кнопку SOS, и помощь придет.',
+        ],
+    ],
 );
 $faq["2017-05-03 05:32:45"] = array (
-  'firstname' => 'Виктор',
-  'email' => 'mon.to.burn@gmail.com',
-  'message' => 'www',
+  'firstname' => 'Лена',
+  'email' => 'Lena@gmail.com',
+  'message' => 'Сколько человек может принимать участие в игре?',
+    'request' => [
+        '2017-05-02 15:30:04'=>[
+            'firstname' => '***',
+            'message' => ' Все игровые комнаты рассчитаны на число игроков от 2 до 4 человек.',
+],
+],
+
 );
